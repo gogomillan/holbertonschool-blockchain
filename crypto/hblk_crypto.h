11312d96dc1727183678a19625cc25f0d63af17e
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2021-03-18 04:03:45 +0000

    Task 0: Adding SHA256

--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -1,9 +1,11 @@
 #ifndef gogomillan
+#define gogomillan
 
 #include <openssl/sha.h>
 #include <stddef.h>
 #include <stdint.h>
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH]);
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH]);
 
 #endif /* gogomillan */
--- crypto/sha256.c
+++ crypto/sha256.c
@@ -1,19 +1,30 @@
 #include "hblk_crypto.h"
 
-uint8_t *sha256(int8_t const *s, size_t len, uint8_t digest[SHA256_DIGEST_LENGTH])
+/*
+ * Function that produce a hash from s to digest
+ *
+ * @s: The input
+ * @len: The lenght
+ * @digest: Where the hash will be stored
+ *
+ * Return: the digest or NULL if fails
+ *
+ */
+uint8_t *sha256(int8_t const *s, size_t len,
+				uint8_t digest[SHA256_DIGEST_LENGTH])
 {
 	SHA256_CTX context;
 
 	if (digest == NULL)
 		return (NULL);
 
-	if(!SHA256_Init(&context))
+	if (!SHA256_Init(&context))
 		return (NULL);
 
-	if(!SHA256_Update(&context, (unsigned char*)s, len))
+	if (!SHA256_Update(&context, (unsigned char *)s, len))
 		return (NULL);
 
-	if(!SHA256_Final(digest, &context))
+	if (!SHA256_Final(digest, &context))
 		return (NULL);
 
 	return (digest);
