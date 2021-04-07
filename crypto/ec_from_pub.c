983d90bbece4d9c8e81c40724a1b8f093224d819
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2021-04-07 22:25:53 +0000

    Taks 3: From public key to ECC_KEY

--- crypto/ec_from_pub.c
+++ crypto/ec_from_pub.c
@@ -9,32 +9,36 @@
  */
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN])
 {
-	EC_KEY *key;
+	EC_KEY *ecckey;
 	EC_POINT *point;
 
 	if (pub == NULL)
 		return (NULL);
 
+	/* Initialize OpenSSL */
 	OpenSSL_add_all_algorithms();
-	/*Create a key from the curve name*/
-	key = EC_KEY_new_by_curve_name(EC_CURVE);
-	if (!key)
+	ERR_load_BIO_strings();
+	ERR_load_crypto_strings();
+	/* Get an ECC_KEY */
+	ecckey = EC_KEY_new_by_curve_name(EC_CURVE);
+	if (!ecckey)
 		return (NULL);
-	/*Create a point*/
-	point = EC_POINT_new(EC_KEY_get0_group(key));
+	/* Get an ECC_PINT from ECC_KEY */
+	point = EC_POINT_new(EC_KEY_get0_group(ecckey));
 	if (!point)
 	{
-		EC_KEY_free(key);
+		EC_KEY_free(ecckey);
 		return (NULL);
 	}
-	/*creates an EC_KEY structure given a public key*/
-	if (!EC_POINT_oct2point(EC_KEY_get0_group(key), point, pub, EC_PUB_LEN, NULL)
-		|| !EC_KEY_set_public_key(key, point))
+	/* Populate EC_KEY structure from the public key*/
+	if (!EC_POINT_oct2point(EC_KEY_get0_group(ecckey), point, pub, EC_PUB_LEN, NULL)
+		|| !EC_KEY_set_public_key(ecckey, point))
 	{
-		EC_KEY_free(key);
+		EC_KEY_free(ecckey);
 		EC_POINT_free(point);
 		return (NULL);
 	}
 	EC_POINT_free(point);
-	return (key);
+
+	return (ecckey);
 }
