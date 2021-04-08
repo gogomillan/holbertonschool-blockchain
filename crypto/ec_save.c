4d61ca9f30a450db854e700aee7788b5b265bd87
Gonzalo Gomez Millan <gonzalo.gomez.millan@gmail.com>
2021-04-08 02:54:22 +0000

    Taks 4: EC_KEY - Save to file

---
+++ crypto/ec_save.c
@@ -0,0 +1,45 @@
+#include "hblk_crypto.h"
+
+/**
+ * ec_save - Function that saves an existing EC key pair on the disk.
+ * @key: The EC_KEY to be save on disk
+ * @folder: The path where the keys should be saved
+ * Return: 1 or 0 upon success or failure respectively
+ */
+int ec_save(EC_KEY *key, char const *folder)
+{
+	FILE *fp;
+	char path[256];
+
+	if (key == NULL || folder == NULL)
+		return (0);
+
+	/* Make a dir if cecessary */
+	mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	/* PATH to the private key file and Open it */
+	sprintf(path, "%s/%s", folder, KEY_PRI_FILE);
+	fp = fopen(path, "w");
+	if (!fp)
+		return (0);
+	/* Write the private key to the file */
+	if (!PEM_write_ECPrivateKey(fp, key, NULL, NULL, 0, NULL, NULL))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+	/* PATH to the public key file and Open it */
+	sprintf(path, "%s/%s", folder, KEY_PUB_FILE);
+	fp = fopen(path, "w");
+	if (!fp)
+		return (0);
+	/* Write the public key to the file */
+	if (!PEM_write_EC_PUBKEY(fp, key))
+	{
+		fclose(fp);
+		return (0);
+	}
+	fclose(fp);
+
+	return (1);
+}
--- crypto/hblk_crypto.h
+++ crypto/hblk_crypto.h
@@ -9,14 +9,18 @@
 #include <openssl/bn.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <sys/stat.h>
 
 #define EC_CURVE NID_secp256k1
 #define EC_PUB_LEN 65
+#define KEY_PRI_FILE "key.pem"
+#define KEY_PUB_FILE "key_pub.pem"
 
 uint8_t *sha256(int8_t const *s, size_t len,
 				uint8_t digest[SHA256_DIGEST_LENGTH]);
 EC_KEY *ec_create(void);
 uint8_t *ec_to_pub(EC_KEY const *key, uint8_t pub[EC_PUB_LEN]);
 EC_KEY *ec_from_pub(uint8_t const pub[EC_PUB_LEN]);
+int ec_save(EC_KEY *key, char const *folder);
 
 #endif /* gogomillan */
